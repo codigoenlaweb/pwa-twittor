3409df04544708ec214578f41c6ecd18ba93c043
codigoenlaweb <codeenlaweb@gmail.com>
2023-09-17 18:59:16 -0400

    feat: manifest.json

---
+++ js/sw-utils.js
@@ -0,0 +1,10 @@
+const updateCacheDynamic = (dynamicCache, req, res) => {
+  if (res.ok) {
+    return caches.open(dynamicCache).then((cache) => {
+      cache.put(req, res.clone());
+      return res.clone();
+    });
+  } else {
+    return res;
+  }
+};
